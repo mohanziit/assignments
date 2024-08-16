7b49a114b7679ca61f45f40feabf84e0ecdef889
mohan.z@iit.ac.lk <mohan.z@iit.ac.lk>
2024-08-16 13:55:37 +0530

    new project for digital clock

---
+++ digitalclock/src/main/java/com/iit/digitalclock/DigitalclockApplication.java
@@ -0,0 +1,13 @@
+package com.iit.digitalclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalclockApplication.class, args);
+	}
+
+}
---
+++ digitalclock/src/test/java/com/iit/digitalclock/DigitalclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.digitalclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
